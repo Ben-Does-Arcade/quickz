aecb3afc2f8c261ff9474781cefc5b384e50fc95
Ben <bendoesarcade@gmail.com>
2024-09-19 12:47:26 -0400

    Test `set_precision()`

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import quickz
 from quickz import Value, Settings
 
+Settings.auto_eng = False
 Settings.auto_print_precision = True
 
-voltage = Value("500 mV")
-current = Value("134 uA")
+# voltage = Value("500 mV")
+# current = Value("134 uA")
+#
+# resistance = voltage / current
+# resistance.add_base("Ω")
+# print(resistance.base)
+# print(resistance)
 
-resistance = voltage / current
-resistance.add_base("Ω")
-print(resistance)
+v = Value("5.07 mV")
+print(v)
+v_std = v.std
+v_std.set_precision(6)
+print(v_std)
