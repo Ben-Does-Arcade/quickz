435e4086b42591c1afe20c83c4418ac2cae72256
Ben <bendoesarcade@gmail.com>
2024-09-19 10:05:33 -0400

    Use R = V/I test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from quickz import Value
 
 voltage = Value("5 V")
-current = Value("134 mA")
+current = Value("134 uA")
 
 resistance = voltage / current
-
+resistance.add_base("Ω")
 print(resistance)
