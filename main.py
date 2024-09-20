002ddfeed5c01ea9e722dab76a51e2d6aeb0b809
Ben <bendoesarcade@gmail.com>
2024-09-20 11:22:25 -0400

    Test `Phasor` math

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-import quickz
 from quickz import *
 
 Settings.auto_eng = True
 Settings.auto_print_precision = True
 
-# voltage = Value("500 mV")
-# current = Value("134 uA")
-#
-# resistance = voltage / current
-# resistance.add_base("Ω")
-# print(resistance.base)
-# print(resistance)
+a = Value("3 A")
+b = Phasor("5 < -30")
 
-# v = Value("5.07 mV")
-# a = Value("50 nA")
-#
-# print(v)
-# print(a)
-#
-# r = v / a
-# r.set_base("Ω")
-# print(r)
-#
-# r.to_std()
-# print(r)
+print(f"a / b: {a / b}")
+print(f"b / a: {b / a}")
+print(f"a * b: {a * b}")
+print(f"b * a: {b * a}")
+print(f"a + b: {a + b}")
+print(f"b + a: {b + a}")
 
-p = Phasor("34 < 50")
-q = Phasor("2 < 15")
-
-print(p / q)
